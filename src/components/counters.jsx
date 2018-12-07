ed5fe79df3d21a6a2120c262117d156aaefef940
Pete <peter.solomons@hotmail.co.uk>
2018-12-07 12:27:10 +0000

    Learning React

---
+++ src/components/counters.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 1 },
+      { id: 3, value: 2 },
+      { id: 4, value: 3 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleReset} className="btn primary btn-sm m-2">
+          Reset
+        </button>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
