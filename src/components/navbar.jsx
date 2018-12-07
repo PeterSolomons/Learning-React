db771f0bd4a6f355dee4821208ec64a41c84d690
Pete <peter.solomons@hotmail.co.uk>
2018-12-07 12:48:57 +0000

    refactoring using destructoring this.props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
-    );
-  }
-}
+//Stateless functional component
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
